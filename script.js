420c4804e2a0a5d59163bea31d4bba4988019af7
Caua Felipe <cauafelipemn@gmail.com>
2026-07-01 08:50:25 -0300

    Fizalização da seção Tabela Nutricional

--- script.js
+++ script.js
@@ -87,6 +87,7 @@ tl2.from(
         rotate: '-90deg',
         left: '-100%',
         top: '110%',
+        
     },
     'ca'
 );
@@ -129,4 +130,79 @@ tl2.to(
         top: '220%',
     },
     'ca'
+);
+
+var tl3 = gsap.timeline({
+    scrollTrigger:{
+        trigger: '.four',
+        //so comeca quando a classe two comeca a aparecer
+        start: '20% 95%',
+        end: '50% 70%', //FIM DA ANIMACAO(TOPO DA TELA ATINGE 50% DA ALTURA QUANDO ESTIVER 20% DA SECAO .THREE)
+        scrub: true,//Acao sincroniza com o scroll
+        markers: false, //DEPURACAO
+    },
+});
+
+tl3.to(
+    '#cocacola',
+    {
+        
+        top: "155%",
+        left: "10%",
+        rotate: '-720deg',
+        
+    },
+    'cai'
+);
+
+tl3.to(
+    '#pepsi',
+    {
+        top: "155%",
+        left: "35%",
+        rotate: '-720deg',
+    },
+    'cai'
+);
+
+tl3.to(
+    '#fanta',
+        {
+            top: "332%",
+            left: "28%",
+            rotate: '-720deg',
+        },
+        'cai'
+);
+
+tl3.to(
+    '#laranja-cortada',
+        {
+            top: "360%",
+            left: "15%",
+            rotate: '740deg',
+            width: "17%",
+        },
+        'cai'
+);
+
+tl3.to(
+    '#folha',
+    {
+        top:"306%",
+        width: '15%',
+        rotate: "-320deg",
+        left: "65%",
+    
+    },
+);
+
+tl3.to(
+    "#folha2",
+    {
+        left: "21%",
+        top: "305%",
+        rotate: "440deg",
+        width: "10%",
+    },
 );
